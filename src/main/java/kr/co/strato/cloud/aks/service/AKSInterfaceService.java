c5892b82592c244fce0b29c84dcefb831069475a
Yong-Min Kwon <Yong-Min Kwon@LAPTOP-AU0I683Q>
2022-10-31 10:28:05 +0900

    Azure > AKS > provisioning 요청 로직 샘플

--- src/main/java/kr/co/strato/cloud/aks/service/AKSInterfaceService.java
+++ src/main/java/kr/co/strato/cloud/aks/service/AKSInterfaceService.java
@@ -2,8 +2,15 @@ package kr.co.strato.cloud.aks.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.azure.core.exception.AzureException;
+import com.azure.core.util.Context;
+import com.azure.resourcemanager.AzureResourceManager;
+
+import kr.co.strato.cloud.aks.exception.BusinessException;
+import kr.co.strato.cloud.aks.exception.ErrorCode;
 import kr.co.strato.cloud.aks.model.CloudParamDto;
 import kr.co.strato.cloud.aks.model.CloudResponseDto.GetList;
 import kr.co.strato.cloud.aks.model.CreateArg;
@@ -13,7 +20,32 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class AKSInterfaceService {
 
+	@Autowired
+	private AzureCredential azureCredential;
+	
 	public String provisioningCluster(CreateArg arg) {
+		
+		log.debug("[provisioningCluster] start");
+		
+		String clusterName = arg.getClusterName();
+		
+		AzureResourceManager azureResourceManager = azureCredential.getAzureAuth(clientId, clientSecret, tenantId, subscriptionId);
+		
+		log.debug("[provisioningCluster] >>> request cluster create");
+
+		// 3. 생성 요청
+		try {
+			azureResourceManager
+						.kubernetesClusters()
+						.manager()
+						.serviceClient()
+						.getManagedClusters()
+						.createOrUpdate(rgName, clusterName, clusterInner, Context.NONE);
+			
+		} catch (AzureException e) {
+			log.error("[provisioningCluster] >>> Faild cluster create", e);
+			throw new BusinessException(ErrorCode.CLUSTER_CREATE_FAILED);
+		}
 		
 		return null;
 	}
