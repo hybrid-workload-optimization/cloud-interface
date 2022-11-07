000b3169bf8ff7024926642fdadc6955953f6178
Yong-Min Kwon <Yong-Min Kwon@LAPTOP-AU0I683Q>
2022-11-07 17:30:01 +0900

    Azure > AKS > 설정에 필요한값 정리

--- src/main/java/kr/co/strato/cloud/aks/service/ProfileSetting.java
+++ src/main/java/kr/co/strato/cloud/aks/service/ProfileSetting.java
@@ -1,26 +1,43 @@
 package kr.co.strato.cloud.aks.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.azure.resourcemanager.containerservice.fluent.models.ManagedClusterInner;
 import com.azure.resourcemanager.containerservice.models.ContainerServiceNetworkProfile;
 import com.azure.resourcemanager.containerservice.models.ManagedClusterAgentPoolProfile;
 
+import kr.co.strato.cloud.aks.model.CreateArg;
+import kr.co.strato.cloud.aks.model.CreateArg.NodePools;
+
 public class ProfileSetting {
 
-	public List<ManagedClusterAgentPoolProfile> agentPoolProfileSet() {
+	public List<ManagedClusterAgentPoolProfile> agentPoolProfileSet(CreateArg arg) {
 		
-		return null;
+		List<ManagedClusterAgentPoolProfile> agentPools = new ArrayList<>();
+		
+		
+		return agentPools;
 		
 	}
 	
 	public ContainerServiceNetworkProfile networkProfileSet() {
 		
+		String dnsPrefix = "testDns";
+		
 		return null;
 	}
 	
-	public ManagedClusterInner clusterInnerSet() {
+	public ManagedClusterInner clusterInnerSet(CreateArg arg) {
 	
+		String clusterName = arg.getClusterName();
+		String kubernetesVersion = arg.getKubernetesVersion();
+		String region = arg.getRegion();
+		List<NodePools> nodePools = arg.getNodePools();
+		
+		List<ManagedClusterAgentPoolProfile> agentPools = agentPoolProfileSet(arg);
+		ContainerServiceNetworkProfile network = networkProfileSet();
+		
 		return null;
 	}
 }
