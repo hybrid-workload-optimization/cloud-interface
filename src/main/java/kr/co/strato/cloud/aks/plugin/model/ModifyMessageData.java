fbc547433d653632585d9c0e0533abf570ea8f01
Yong-Min Kwon <Yong-Min Kwon@LAPTOP-AU0I683Q>
2022-11-17 18:23:55 +0900

    Azure > AKS > Job 타입별 파라메타 추가

--- src/main/java/kr/co/strato/cloud/aks/plugin/model/ModifyMessageData.java
+++ src/main/java/kr/co/strato/cloud/aks/plugin/model/ModifyMessageData.java
@@ -1,5 +1,20 @@
 package kr.co.strato.cloud.aks.plugin.model;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import kr.co.strato.cloud.aks.model.CloudParamDto.ModifyArg;
+
 public class ModifyMessageData {
 
+	@Positive(message = "workJobIdx 는 null 또는 0을 허용하지 않으며, 양수만 허용합니다.")
+	private Long workJobIdx;
+	@NotEmpty(message = "callbackUrl 은 null 또는 공백을 허용하지 않습니다.")
+	private String callbackUrl;
+	@NotEmpty(message = "jobType 은 null 또는 공백을 허용하지 않습니다. (scale)")
+	private String jobType;
+	@NotNull(message = "param 은 null 허용하지 않습니다.")
+	private ModifyArg param;
+	
 }
